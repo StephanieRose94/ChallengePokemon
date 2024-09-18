fbfd13a1e09408ead739274f371b00e9b5fba3fb
steff09 <steffany@glya.co>
2024-09-18 15:33:26 -0500

    initial configurations and connection to API

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="pokemon in pokemons" :key="pokemon.name">
+                {{ pokemon.name }}
+            </li>
+        </ul>
+    </div>
+    
+</template>
+<script>
+    import axios from "axios";
+
+    export default{
+        data() {
+            return {
+                pokemons: [],
+            }
+        },
+        mounted() {
+            this.getPokemons()
+        },
+        methods: {
+            getPokemons(){
+                axios.get('https://pokeapi.co/api/v2/pokemon').then(res => {
+                    this.pokemons = res.data.results
+                }).catch(error => {
+                    console.log(error);
+                })
+            }
+        },
+    }
+</script>
+<style>
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store(
+    {
+        state: {
+            favorites: []
+        },
+        mutations: {
+            ADD_FAVORITE(state,pokemon){
+                state.favorites.push(pokemon)
+            },
+            REMOVE_FAVORITE(state,pokemon){
+                state.favorites = state.favorites.filter(p => p.name !== pokemon);
+            }
+        },
+        getters: {
+            getFavorites: state => state.favorites
+        }
+    }
+)
